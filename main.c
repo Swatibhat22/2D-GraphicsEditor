f0d8e09337116b4996c1ab1efcf0f62734d4bd5c
Swatibhat22 <swatibhat226@gmail.com>
2026-06-05 23:32:16 +0530

    Added basic line drwing feature

--- main.c
+++ main.c
@@ -4,10 +4,13 @@
 #define COLS 60
 
 char canvas[ROWS][COLS];
+void initializeCanvas();
+void displayCanvas();
+void clearCanvas();
 
 void initializeCanvas(){
     int i,j;
-    for(int i=0; i<ROWS; i++){
+    for(i=0; i<ROWS; i++){
         for(j=0; j<COLS; j++){
             canvas[i][j]=' ';
         }
@@ -39,6 +42,9 @@ void clearCanvas(){
     printf("\nCanvas Cleared Successfully!\n");
 }
 
+void plotPoint(int x, int y);
+void drawLine(int x1, int y1, int x2, int y2);
+
 int main()
 {
     int choice;
@@ -51,32 +57,80 @@ int main()
         printf("===================================\n");
         printf("       ASCII GRAPHICS EDITOR\n");
         printf("===================================\n");
-        printf("1. Display Canvas\n");
-        printf("2. Clear Canvas\n");
-        printf("3. Exit\n");
+        printf("1. Draw Line\n");
+        printf("2. Display Canvas\n");
+        printf("3. Clear Canvas\n");
+        printf("4. Exit\n");
         printf("Enter Choice : ");
 
         scanf("%d", &choice);
 
         switch(choice)
-        {
-            case 1:
-                displayCanvas();
-                break;
+    {
+    case 1:
+    {
+        int x1, y1, x2, y2;
 
-            case 2:
-                clearCanvas();
-                break;
+        printf("Enter Starting Point (row col): ");
+        scanf("%d %d", &x1, &y1);
 
-            case 3:
-                printf("\nProgram Closed.\n");
-                return 0;
+        printf("Enter Ending Point (row col): ");
+        scanf("%d %d", &x2, &y2);
 
-            default:
-                printf("\nInvalid Choice! Try Again.\n");
-        }
+        drawLine(x1, y1, x2, y2);
+
+        printf("Line Drawn Successfully!\n");
+        break;
+    }
+
+    case 2:
+        displayCanvas();
+        break;
+
+    case 3:
+        clearCanvas();
+        break;
+
+    case 4:
+        printf("Program Closed.\n");
+        return 0;
+
+    default:
+        printf("Invalid Choice!\n");
+       }
     }
 
     return 0;
 }
 
+void plotPoint(int x, int y)
+{
+    if(x >= 0 && x < ROWS && y >= 0 && y < COLS)
+    {
+        canvas[x][y] = '*';
+    }
+}
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+
+    float xIncrement = dx / (float)steps;
+    float yIncrement = dy / (float)steps;
+
+    float x = x1;
+    float y = y1;
+
+    int i;
+
+    for(i = 0; i <= steps; i++)
+    {
+        plotPoint((int)x, (int)y);
+
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
